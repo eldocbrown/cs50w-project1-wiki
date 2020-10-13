e336c9e7728becfab1980d34b2744aa5d7594485
Juan Cruz Donato <juandonato@gmail.com>
2020-10-12 23:53:23 -0300

    Search

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,14 +1,55 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 from . import util
 
-# Remove
-from django.http import HttpResponse
+def render_index(request, page_header, entries):
+    return render(request, "encyclopedia/index.html", {
+        "page_header": page_header,
+        "entries": entries
+    })
+
+def search_entries(entries, q):
+    result = []
+    for entry in entries:
+        if q in entry:
+            result.append(entry)
+    return result
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    # If it is a POST request, assume its a Search operation
+    if request.method == "POST":
+        q = request.POST["q"]
+        entries = []
+        page_header = "No articles found"
+        if len(q) > 0:
+            entries = util.list_entries()
+            # Entries found in directory
+            if len(entries) > 0:
+                # Redirect to unique coincidence
+                if q in entries:
+                    return redirect('encyclopedia:entry', entry=q)
+                # Search for substrings
+                else:
+                    search = search_entries(entries, q)
+                    # Entries with substrings found
+                    if len(search) > 0:
+                        page_header = "Search Result"
+                        return render_index(request, page_header, search)
+                    # No articles found
+                    else:
+                        page_header = "No articles found"
+                        return render_index(request, page_header, [])
+            # No entries found in directory
+            else:
+                page_header = "No articles found in the article repository"
+                return render_index(request, page_header, entries)
+        # Blank parameter on input
+        else:
+            return render_index(request, page_header, entries)
+
+    # If it is a GET request, render All Pages
+    else:
+        return render_index(request, "All Pages", util.list_entries())
 
 def entry(request, entry):
     #Try to get entry
