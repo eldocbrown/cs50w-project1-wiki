af56d92f131a67a0a6a9f4359c4f0c1c574770b9
Juan Cruz Donato <juandonato@gmail.com>
2020-10-15 10:43:43 -0300

    Fixed paths so app works under /wiki

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:entry>", views.entry, name="entry"),
-    path("random", views.random, name="random"),
-    path("newpage", views.newpage, name="newpage"),
-    path("editpage/<str:entry>", views.editpage, name="editpage"),
+    path("<str:entry>", views.entry, name="entry"),
+    path("action/random", views.random, name="random"),
+    path("action/newpage", views.newpage, name="newpage"),
+    path("action/editpage/<str:entry>", views.editpage, name="editpage"),
     path("message", views.message, name="message"),
 ]
